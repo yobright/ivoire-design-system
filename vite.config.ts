ce76e73d909cc08f0c95b80dc4cf1b3091453cd8
yobright <mbahiag@gmail.com>
2024-08-31 09:59:34 +0000

    feat: added husky and storybook

---
+++ vite.config.ts
@@ -0,0 +1,28 @@
+/// <reference types="vitest" />
+
+import { defineConfig } from "vite";
+import dts from "vite-plugin-dts";
+import { peerDependencies } from "./package.json";
+
+export default defineConfig({
+  build: {
+    lib: {
+      entry: "./src/index.ts", 
+      name: "ivoiredesignsystem",
+      fileName: (format) => `index.${format}.js`,
+      formats: ["cjs", "es"],
+    },
+    rollupOptions: {
+      external: [...Object.keys(peerDependencies)],
+    },
+    sourcemap: true, 
+    emptyOutDir: true,
+  },
+  plugins: [dts()],
+  test: {
+    globals: true,
+    environment: "jsdom",
+    setupFiles: "./setupTests.ts",
+  },
+});
+
