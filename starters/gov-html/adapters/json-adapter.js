981d5980baaa968548d3c9f9ba8aa11328daf193
kasskoo <mbahiag@gmail.com>
2026-04-25 00:17:11 +0000

    Remove gov-next starter template and associated files

--- starters/gov-html/adapters/json-adapter.js
+++
@@ -1,7 +0,0 @@
-export async function loadLanding(locale = 'fr') {
-  const response = await fetch(`./data/landing.${locale}.json`);
-  if (!response.ok) {
-    throw new Error(`Unable to load landing content for locale: ${locale}`);
-  }
-  return response.json();
-}
